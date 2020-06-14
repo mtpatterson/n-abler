0d1a1b0a153d44e0e7e2237359a9b2166f751a60
Brandon Patterson <brandnpatterson@gmail.com>
2020-06-14 01:06:50 -0500

    remove log

--- src/js/search/useEndlessScroll.js
+++ src/js/search/useEndlessScroll.js
@@ -34,8 +34,6 @@ export default function useEndlessScroll(
           );
 
           setResults(results.concat(posts));
-
-          console.log(results.concat(posts));
           setLoading(false);
         }
       }
